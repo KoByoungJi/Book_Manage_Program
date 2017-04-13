79479ed376aa265c8904774f3680aa0d36fc1352
KoByoungJi <kbj933812@naver.com>
2017-04-13 21:54:24 +0900

    Upload Book.java

---
+++ Book.java
@@ -0,0 +1,33 @@
+package °íº´Áö_30202;
+
+public class Book {
+	private String Name;
+	private String Author;
+	private String Publisher;
+	private String Cost;
+	
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getAuthor() {
+		return Author;
+	}
+	public void setAuthor(String author) {
+		Author = author;
+	}
+	public String getPublisher() {
+		return Publisher;
+	}
+	public void setPublisher(String publisher) {
+		Publisher = publisher;
+	}
+	public String getCost() {
+		return Cost;
+	}
+	public void setCost(String cost) {
+		Cost = cost;
+	}	
+}
